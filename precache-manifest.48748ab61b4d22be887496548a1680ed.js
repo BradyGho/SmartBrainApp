22ae3ff970fcada630c06bf0b537116fb3205a08
MyNameIsNotDerek <bradygho0130@gmail.com>
2019-12-29 13:59:58 -0800

    Updates

---
+++ precache-manifest.48748ab61b4d22be887496548a1680ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bbbbb86f33c6bcd8262364b384de5f",
+    "url": "/SmartBrainApp/index.html"
+  },
+  {
+    "revision": "daf8ec4877f7e00b4608",
+    "url": "/SmartBrainApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cedbc95310e3d8faae0d",
+    "url": "/SmartBrainApp/static/css/main.8313f626.chunk.css"
+  },
+  {
+    "revision": "daf8ec4877f7e00b4608",
+    "url": "/SmartBrainApp/static/js/2.2c05ab64.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SmartBrainApp/static/js/2.2c05ab64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cedbc95310e3d8faae0d",
+    "url": "/SmartBrainApp/static/js/main.24a95ee2.chunk.js"
+  },
+  {
+    "revision": "a11620526ee0e9a0be89",
+    "url": "/SmartBrainApp/static/js/runtime-main.9b83f4b4.js"
+  },
+  {
+    "revision": "45a2f0b8657a330a8838fc65fbf32ac9",
+    "url": "/SmartBrainApp/static/media/brain.45a2f0b8.png"
+  }
+]);
